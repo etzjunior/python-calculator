ea29a8cd6e9791bd0715b54ce4ad9d693f7db2fe
etzjunior <victorwoha233@gmail.com>
2025-08-10 04:04:51 +0200

    Implemented a loop and Calculation chanining

--- MyCalcProject.py
+++ MyCalcProject.py
@@ -1,19 +1,8 @@
-num1 = float(input("Enter number: "))
-
-print("Select Operations:")
-print("+. Add")
-print("-. Subract")
-print("/. Divide")
-print("*. Multiply")
-
-Choice = input("Enter Your Choice: ")
-
-num2 = float(input("Enter another number: "))
-
-print("First number: ", num1)
-print("Second number:", num2)
-
 def calculate(num1, num2, Choice):
+    valid_choices = ['+', '-', '*', '/']
+    if Choice not in valid_choices:
+        return "Invalid Input"
+    
     if Choice == '+':
         return num1 + num2
     elif Choice == '-':
@@ -25,7 +14,39 @@ def calculate(num1, num2, Choice):
     elif Choice == '*':
         return num1 * num2
     else:
-        return "Invalid input"
+        return "Invalid input"  
+
+calculator_active = True
+continuing = False
+
+while calculator_active:
+    if continuing:
+        num1 = Current_result
+        continuing = False
+    else:
+        num1 = float(input("Enter number: "))
+
+    print("Select Operations:")
+    print("+. Add")
+    print("-. Subract")
+    print("/. Divide")
+    print("*. Multiply")
+
+    Choice = input("Enter Your Choice: ")
+
+    num2 = float(input("Enter another number: "))
+
+    print("First number: ", num1)
+    print("Second number:", num2)
     
-result = calculate(num1, num2, Choice)
-print(result)
+    result = calculate(num1, num2, Choice)
+    print(result)
+     
+    Current_result = result
+    print("Do you wanna continue calculating?")
+    Choice2 = input("input y/n: ")
+
+    if Choice2 == "y":
+        continuing = True
+
+        
